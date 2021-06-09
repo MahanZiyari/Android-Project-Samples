071336559665cc1df943a08a09f4c6287f87d476
Mahan Ziyari <maziyar.mz.021@gmail.com>
2021-06-09 09:26:45 +0430

    Update PickUp and Summary fragment

---
+++ android-basics-kotlin-cupcake-app-starter/app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -0,0 +1,64 @@
+package com.example.cupcake.model
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.text.SimpleDateFormat
+import java.util.*
+
+class OrderViewModel : ViewModel() {
+    // properties
+    private val _quantity = MutableLiveData<Int>()
+    val quantity: LiveData<Int> get() = _quantity
+
+    private val _flavor = MutableLiveData<String>()
+    val flavor: LiveData<String> get() = _flavor
+
+    private val _date = MutableLiveData<String>()
+    val date: LiveData<String> get() = _date
+
+    private val _price = MutableLiveData<Double>()
+    val price: LiveData<Double> = _price
+
+    val dateOptions = getPickupOptions()
+
+    init {
+        resetOrder()
+    }
+
+    fun setQuantity(numberCupcakes: Int) {
+        _quantity.value = numberCupcakes
+    }
+
+    fun setFlavor(desiredFlavor: String) {
+        _flavor.value = desiredFlavor
+    }
+
+    fun setDate(pickupDate: String) {
+        _date.value = pickupDate
+    }
+
+    fun hasNoFlavorSet(): Boolean {
+        return _flavor.value.isNullOrEmpty()
+    }
+
+    fun resetOrder() {
+        _quantity.value = 0
+        _flavor.value = ""
+        _date.value = dateOptions[0]
+        _price.value = 0.0
+    }
+
+    private fun getPickupOptions(): List<String> {
+        val options = mutableListOf<String>()
+        val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
+        val calendar = Calendar.getInstance()
+
+        repeat(4){
+            options.add(formatter.format(calendar.time))
+            calendar.add(Calendar.DATE, 1)
+        }
+
+        return options
+    }
+}
