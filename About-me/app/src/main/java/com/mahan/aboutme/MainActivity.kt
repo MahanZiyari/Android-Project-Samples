96248ea78eafd9aa43242ecfd033225030d5866a
Mahan Ziyari <maziyar.mz.021@gmail.com>
2021-06-22 16:10:22 +0430

    Implement Data Binding

--- About-me/app/src/main/java/com/mahan/aboutme/MainActivity.kt
+++ About-me/app/src/main/java/com/mahan/aboutme/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.mahan.aboutme
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.mahan.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    // Instance of MyName data class.
+    private val myName: MyName = MyName("Mahan Ziyari")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
+
+        binding.doneButton.setOnClickListener { addNickName(it) }
+    }
+
+    fun addNickName(view: View){
+        myName?.nickName = binding.nicknameEdit.text.toString()
+
+        binding.apply {
+            nicknameText.text = myName?.nickName
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+
+        hideKeyBoard(view)
+    }
+
+    private fun hideKeyBoard(view: View) {
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE)
+                as InputMethodManager
+
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
---
+++ About-me/app/src/main/java/com/mahan/aboutme/MyName.kt
@@ -0,0 +1,3 @@
+package com.mahan.aboutme
+
+data class MyName(var name: String = "", var nickName: String = "")
